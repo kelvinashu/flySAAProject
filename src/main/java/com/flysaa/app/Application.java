b76dc41de6c78ac92de49c7be2eec7f1306a50e2
Kabelo Lucas Kekana <kabelolu86@gmail.com>
2020-08-21 11:54:03 +0200

    added my name in application class

--- src/main/java/com/flysaa/app/Application.java
+++ src/main/java/com/flysaa/app/Application.java
@@ -1,8 +1,19 @@
 public class Application{
 
 public static void main(String [] abc){
+<<<<<<< HEAD
 System.out.println("Hello Kabelo");
+<<<<<<< HEAD
 System.out.println("Kabelo Kekana");
+=======
+System.out.println("Hello Njungwoh");
+=======
+System.out.println("Hello Bef");
+>>>>>>> 44798d962f9787a7f1a1ccc805a399bcd9f5f5aa
+System.out.println("Hello Segunabddul");
+>>>>>>> eaa786231c91fbd232c0b562490bc080a70ef874
+System.out.println("Kabelolu");
+
 }
 
 }
